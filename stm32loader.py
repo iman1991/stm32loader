a4d2a29d43ee3a98c7e8538a96b23a0d25129746
iman1991 <iman1991@mail.ru>
2017-09-09 14:08:28 +0300

    to python3

--- stm32loader.py
+++ stm32loader.py
@@ -387,7 +387,7 @@ if __name__ == "__main__":
         opts, args = getopt.getopt(sys.argv[1:], "hqVewvrp:b:a:l:g:")
     except getopt.GetoptError as err:
         # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        print(str(err)) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
 
